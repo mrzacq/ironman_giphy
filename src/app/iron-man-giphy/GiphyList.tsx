83be0af4d36f88145383e75765e2bbe357dcb39c
mrzacq <hartantoaanan@gmail.com>
2023-07-20 09:44:32 +0700

    feat: add type on mapping

--- src/app/iron-man-giphy/GiphyList.tsx
+++ src/app/iron-man-giphy/GiphyList.tsx
@@ -1,12 +1,12 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import React from 'react';
 
 import GiphyItem from '@/app/iron-man-giphy/GiphyItem';
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export default function GiphyList({ data }: { data: any }) {
   return (
     <div className='grid grid-cols-1 gap-8 lg:grid-cols-3'>
-      {data?.map((giphy, i) => {
+      {data?.map((giphy: any, i: number) => {
         return <GiphyItem giphy={giphy} key={i} />;
       })}
     </div>
