a8f13b715da56e3f4b4cedf854318e57a2476b6e
mrzacq <hartantoaanan@gmail.com>
2023-07-20 09:52:23 +0700

    feat: change site title

--- src/constant/config.ts
+++ src/constant/config.ts
@@ -1,7 +1,6 @@
 export const siteConfig = {
-  title: 'Next.js + Tailwind CSS + TypeScript Starter',
-  description:
-    'A starter for Next.js, Tailwind CSS, and TypeScript with Absolute Import, Seo, Link component, pre-configured with Husky',
+  title: 'GIPHY | Anan Hartanto',
+  description: 'A simple giphy application for searching ur favorite gif image',
   /** Without additional '/' on the end, e.g. https://theodorusclarence.com */
-  url: 'https://tsnext-tw.thcl.dev',
+  url: 'https://giphy.ananhartanto.com',
 };
